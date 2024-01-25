f22f0751872fee4109233b92e2484bcae0e80e8c
miran-nihad <miran-walashy@hotmail.com>
2024-01-25 23:14:43 +0100

    Arbeidskrav_2
    av Miran Nihad

---
+++ arbeidskrav_2/ressurser.js
@@ -0,0 +1,136 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+const faner = {
+    HTML: resources.filter((html) => html.category === "HTML"),
+    CSS: resources.filter((css) => css.category === "CSS"),
+    JavaScript: resources.filter((js) => js.category === "JavaScript"),
+    React: resources.filter((react) => react.category === "React"),
+    "Sanity and headless CMS": resources.filter((Sanity) => Sanity.category === "Sanity and headless CMS")
+}
+
+const kategorier = document.getElementById("innhold")
+const buttons = document.querySelectorAll("#container button");
+
+function updateContent(category) {
+    const selectedResources = faner[category];
+    kategorier.innerHTML = selectedResources.map(resource => {
+      const links = resource.sources.map(link => `<li><a target="_blank" href="${link.url}">${link.title}</a></li>`).join("");
+      return `<h2>${resource.category}</h2><p>${resource.text}</p><ul>${links}</ul>`;
+    }).join("");
+
+    buttons.forEach(btn => btn.classList.toggle("active", btn.id === category + "Btn"));
+    //document.querySelector(`[data-category="${category}"]`).classList.add("active");
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const category = button.textContent.trim(); // Remove any leading/trailing whitespace
+      updateContent(category);
+    });
+  });
+   updateContent("HTML");
+
+
+
+
+
+
+
+
+
+    
